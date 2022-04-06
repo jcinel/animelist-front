1d0de7503603ce4ebd2a7cbc8a11bb9e244321be
jcinel <julia.cinel@estudante.ufscar.br>
2022-04-05 23:26:41 -0300

    colocando botão de criar anime/usuário

--- src/app/modules/top-bar/top-bar.component.ts
+++ src/app/modules/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class TopBarComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  displayAddAnime: boolean = false;
+
+  @Input()
+  displayAddUsuario: boolean = false;
 
   ngOnInit(): void {
   }
@@ -25,5 +31,15 @@ export class TopBarComponent implements OnInit {
   paginaUsuarios(){
     const baseUrl = window.location.href.split("/", 1)[0];
     window.location.href = baseUrl.concat("/usuarios/listar");
+  }
+
+  paginaAnimesCriar(){
+    const baseUrl = window.location.href.split("/", 1)[0];
+    window.location.href = baseUrl.concat("/animes/criar");
+  }
+
+  paginaUsuariosCriar(){
+    const baseUrl = window.location.href.split("/", 1)[0];
+    window.location.href = baseUrl.concat("/usuarios/criar");
   }
 }
