02d27785391dd659ab7d0d5b225ce2f74bc1d3c3
jcinel <julia.cinel@estudante.ufscar.br>
2022-04-06 22:27:43 -0300

    Adicionando componente de detalhar usuário

--- src/app/modules/detalhar-usuarios/detalhar-usuarios.component.ts
+++ src/app/modules/detalhar-usuarios/detalhar-usuarios.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-detalhar-usuarios',
   templateUrl: './detalhar-usuarios.component.html',
   styleUrls: ['./detalhar-usuarios.component.css']
 })
-export class DetalharUsuariosComponent implements OnInit {
+export class DetalharUsuariosComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  usuario: Usuario = {
+    id: -1,
+    nome: '',
+    dataNasc: '',
+    email: ''
+  }
+
+  @Input()
+  id: number = -1;
+
+  constructor(private usuariosService: UsuariosService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.detalharUsuarios(this.id);
+  }
 
   ngOnInit(): void {
   }
 
   detalharUsuarios(id: number){
-    
+    this.usuariosService.detalharUsuarios(id).subscribe({
+      next: usuario => {
+        this.usuario = usuario;
+      },
+      error: err => {
+        console.log('Erro ao encontrar o usuário', err);
+      }
+    })
   }
-
 }
